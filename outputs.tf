343b8b0b136d2e19a409c246e16bb2800ff30347
tbsantos <tiagobs.santos@gmail.com>
2025-05-20 06:20:13 -0300

    Criando Website

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,13 @@ output "aws_vpc" {
   description = ""
   value       = aws_vpc.this.id
 }
+
+output "sg_elb" {
+  description = ""
+  value       = aws_security_group.sg_elb
+}
+
+output "sg_srv" {
+  description = ""
+  value       = aws_security_group.sg_srv
+}
